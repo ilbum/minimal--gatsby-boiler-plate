d65b2af4e1548fb0fcb36c4e92378718f2bdd9fb
Ilbum Kwak <ilbum@nbranchconsulting.com>
2021-12-30 00:09:41 -0800

    footer, layout, and scss updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,24 @@
 import * as React from 'react';
 import Navbar from './navbar';
+import Footer from './footer';
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className="root-container-flex">
       <title>{pageTitle}</title>
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
       <div id="main-container">
         <main>
-          <h1>{pageTitle}</h1>
-          {children}
+          {/* hero section */}
+          <div id="hero-div">
+            <h1>{pageTitle}</h1>
+          </div>
+          {/* content section */}
+          <div id="content-div">{children}</div>
         </main>
+        <Footer />
       </div>
     </div>
   );
